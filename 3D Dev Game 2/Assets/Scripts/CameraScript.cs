4967c25749f1744b7e53c7f3c69048d0b30641f2
DASUltima <36460671+DASUltima@users.noreply.github.com>
2018-05-04 09:43:44 -0400

    New Update

---
+++ 3D Dev Game 2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public static CameraScript Instance;
+
+    public float zLimit;
+    public float zNegLimit;
+    public float xLimit;
+    public float xNegLimit;
+    public float cameraMoveSpeed;
+    public float panBorderThickness;
+
+    public Vector3 pivot;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        pivot = transform.position;
+    }
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        pivot += new Vector3(horizontal * cameraMoveSpeed, 0, vertical * cameraMoveSpeed);
+
+        transform.position = pivot;
+    }
+}
---
+++ 3D Dev Game 2/Assets/Scripts/CanvasScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 targetPostition = new Vector3(Camera.main.transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        transform.LookAt(targetPostition);
+    }
+}
