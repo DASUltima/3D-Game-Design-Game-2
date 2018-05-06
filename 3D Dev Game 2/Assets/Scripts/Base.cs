81fe59db6b4f2d2eed35ef41afa1ce84410ad0ce
NolanH96 <harris.nolan@gmail.com>
2018-05-06 15:38:23 -0400

    Test Push

---
+++ 3D Dev Game 2/Assets/Scripts/Base.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour {
+
+    public Faction faction;
+    public List<Base> connectingBase = new List<Base>();
+    public Stat health;
+    public MeshRenderer meshRenderer;
+    public MeshRenderer auraRenderer;
+
+    private void Start()
+    {
+        health.Initialize();
+    }
+    public void TakeDamage(int damage)
+    {
+        if (damage < health.CurrentVal)
+        {
+            health.CurrentVal -= damage;
+        }
+        else
+            health.CurrentVal = 0;
+    }
+}
+public enum Faction {Player,Enemy,None}
