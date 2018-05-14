347de4cb6d88150ce57b255e978eb809cb6a9ee6
DASUltima <36460671+DASUltima@users.noreply.github.com>
2018-05-14 13:23:44 -0400

    New

---
+++ 3D Dev Game 2/Assets/Scripts/GameManager.cs
@@ -0,0 +1,228 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+
+    public static GameManager Instance;
+
+    [Header("Player")]
+    public GameObject unitMenu;
+    public LayerMask playerBaseLayer;
+    public List<GameObject> units;
+    public Stat playerUnitCooldown1;
+    public Stat playerUnitCooldown2;
+    public Stat playerUnitCooldown3;
+    public Base currentBase;
+    public Base playerHomeBase;
+
+    public Base destinationBase;
+
+    LineRenderer lineRenderer;
+    int currentPath;
+
+    [Header("Enemy")]
+    public Base enemyHomeBase;
+    public List<GameObject> enemyUnits;
+    public float enemyUnitCooldown;
+    public List<Base> enemyBases;
+    bool gameEnded;
+
+    public GameObject gameEndMenu;
+    public Material playerBaseMaterial;
+    public Material playerAuraMaterial;
+    public Material enemyBaseMaterial;
+    public Material enemyAuraMaterial;
+    public Sprite loseSprite;
+    public Sprite winSprite;
+    public AudioClip win;
+    public AudioClip lose;
+
+    // Use this for initialization
+    void Start()
+    {
+        Instance = this;
+        lineRenderer = GetComponent<LineRenderer>();
+        playerUnitCooldown1.Initialize();
+        playerUnitCooldown2.Initialize();
+        playerUnitCooldown3.Initialize();
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyHomeBase.health.CurrentVal == 0 && !gameEnded)
+        {
+            gameEnded = true;
+            gameEndMenu.SetActive(true);
+            gameEndMenu.GetComponent<Image>().sprite = winSprite;
+            GetComponent<AudioSource>().clip = win;
+            GetComponent<AudioSource>().Play();
+        }
+        else if (playerHomeBase.health.CurrentVal == 0 && !gameEnded)
+        {
+            gameEnded = true;
+            gameEndMenu.SetActive(true);
+            gameEndMenu.GetComponent<Image>().sprite = loseSprite;
+            GetComponent<AudioSource>().clip = lose;
+            GetComponent<AudioSource>().Play();
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (playerUnitCooldown1.CurrentVal >= playerUnitCooldown1.MaxVal)
+            {
+                GameObject testunit = Instantiate(units[0], currentBase.transform.position, Quaternion.identity);
+                testunit.GetComponent<NavMeshAgent>().SetDestination(currentBase.connectingBase[currentPath].transform.position);
+                testunit.GetComponent<Unit>().destination = currentBase.connectingBase[currentPath];
+                testunit.GetComponent<Unit>().spawnBase = currentBase;
+                playerUnitCooldown1.CurrentVal = 0;
+                playerUnitCooldown1.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+                playerUnitCooldown2.CurrentVal = 0;
+                playerUnitCooldown2.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+                playerUnitCooldown3.CurrentVal = 0;
+                playerUnitCooldown3.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            if (playerUnitCooldown1.CurrentVal >= playerUnitCooldown1.MaxVal)
+            {
+                GameObject testunit = Instantiate(units[0], currentBase.transform.position, Quaternion.identity);
+                testunit.GetComponent<NavMeshAgent>().SetDestination(currentBase.connectingBase[currentPath].transform.position);
+                testunit.GetComponent<Unit>().destination = currentBase.connectingBase[currentPath];
+                testunit.GetComponent<Unit>().spawnBase = currentBase;
+                playerUnitCooldown1.CurrentVal = 0;
+                playerUnitCooldown1.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+                playerUnitCooldown2.CurrentVal = 0;
+                playerUnitCooldown2.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+                playerUnitCooldown3.CurrentVal = 0;
+                playerUnitCooldown3.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+            }
+        }
+        EnemyAI();
+        playerUnitCooldown1.CurrentVal += Time.deltaTime;
+        playerUnitCooldown2.CurrentVal += Time.deltaTime;
+        playerUnitCooldown3.CurrentVal += Time.deltaTime;
+        for (int buttonIndex = 0; buttonIndex < unitMenu.transform.childCount; buttonIndex++)
+        {
+            if (playerUnitCooldown1.CurrentVal < playerUnitCooldown1.MaxVal)
+                unitMenu.transform.GetChild(buttonIndex).GetComponent<Button>().interactable = false;
+            else if (playerUnitCooldown1.CurrentVal >= playerUnitCooldown1.MaxVal)
+                unitMenu.transform.GetChild(buttonIndex).GetComponent<Button>().interactable = true;
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                // Shoot out a ray
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+                // If we hit
+                if (Physics.Raycast(ray, out hit, 100, playerBaseLayer))
+                {
+                    unitMenu.SetActive(true);
+                    if (currentBase)
+                        currentBase.selector.SetActive(false);
+                    currentBase = hit.collider.GetComponent<Base>();
+                    currentBase.selector.SetActive(true);
+                    destinationBase = currentBase.connectingBase[0];
+                    lineRenderer.enabled = true;
+                    DrawPathBetweenBases();
+                }
+                else if (!Physics.Raycast(ray, out hit, 100, playerBaseLayer))
+                {
+                    unitMenu.SetActive(false);
+                    if (currentBase)
+                        currentBase.selector.SetActive(false);
+                    currentBase = null;
+                    destinationBase = null;
+                    currentPath = 0;
+                    lineRenderer.enabled = false;
+                }
+            }
+        }
+        if (currentBase)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+                if (currentPath > 0)
+                {
+                    currentPath--;
+                    destinationBase = currentBase.connectingBase[currentPath];
+                }
+                else
+                {
+                    destinationBase = currentBase.connectingBase[currentBase.connectingBase.Count - 1];
+                    currentPath = currentBase.connectingBase.Count - 1;
+                }
+                DrawPathBetweenBases();
+            }
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (currentPath + 1 < currentBase.connectingBase.Count)
+                {
+                    currentPath++;
+                    destinationBase = currentBase.connectingBase[currentPath];
+                }
+                else
+                {
+                    destinationBase = currentBase.connectingBase[0];
+                    currentPath = 0;
+                }
+                DrawPathBetweenBases();
+            }
+        }
+    }
+    public void ButtonMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void ButtonSpawnUnit()
+    {
+        int unitIndex = EventSystem.current.currentSelectedGameObject.transform.GetSiblingIndex();
+        GameObject testunit = Instantiate(units[unitIndex], currentBase.transform.position, Quaternion.identity);
+        testunit.GetComponent<NavMeshAgent>().SetDestination(currentBase.connectingBase[currentPath].transform.position);
+        testunit.GetComponent<Unit>().destination = currentBase.connectingBase[currentPath];
+        testunit.GetComponent<Unit>().spawnBase = currentBase;
+        playerUnitCooldown1.CurrentVal = 0;
+        playerUnitCooldown1.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+        playerUnitCooldown2.CurrentVal = 0;
+        playerUnitCooldown2.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+        playerUnitCooldown3.CurrentVal = 0;
+        playerUnitCooldown3.MaxVal = units[0].GetComponent<Unit>().spawnCooldown;
+    }
+    void DrawPathBetweenBases()
+    {
+        lineRenderer.SetPosition(0, currentBase.transform.position);
+        lineRenderer.SetPosition(1, destinationBase.transform.position);
+    }
+    void EnemyAI()
+    {
+        enemyUnitCooldown -= Time.deltaTime;
+        if (enemyUnitCooldown < 0)
+        {
+            int currentBase = Random.Range(0, enemyBases.Count);
+            int currentUnit = Random.Range(0, enemyUnits.Count);
+            Base destinationBase = null;
+            List<Base> possibleTargets = new List<Base>();
+            for (int pathIndex = 0; pathIndex < enemyBases[currentBase].connectingBase.Count; pathIndex++)
+                if (enemyBases[currentBase] && enemyBases[currentBase].connectingBase[pathIndex] && enemyBases[currentBase].connectingBase[pathIndex].faction == Faction.None || enemyBases[currentBase].connectingBase[pathIndex].faction == Faction.Player)
+                    possibleTargets.Add(enemyBases[currentBase].connectingBase[pathIndex]);
+
+            int target = Random.Range(0, possibleTargets.Count);
+            destinationBase = possibleTargets[target];
+
+            GameObject testunit = Instantiate(enemyUnits[currentUnit], enemyBases[currentBase].transform.position, Quaternion.identity);
+            testunit.GetComponent<NavMeshAgent>().SetDestination(destinationBase.transform.position);
+            testunit.GetComponent<Unit>().spawnBase = enemyBases[currentBase];
+            testunit.GetComponent<Unit>().destination = destinationBase;
+            enemyUnitCooldown = enemyUnits[currentUnit].GetComponent<Unit>().spawnCooldown;
+        }
+    }
+}
---
+++ 3D Dev Game 2/Assets/Scripts/Unit.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Unit : MonoBehaviour {
+
+    public Faction faction;
+    public float speed;
+    public float spawnCooldown;
+
+    public Unit target;
+    public int attackDamage;
+    public float attackCooldown;
+    float nextAttack;
+    public Stat health;
+    public LayerMask canSee;
+    public float sightRange;
+    RaycastHit hit;
+    public Base destination;
+    public Base spawnBase;
+
+    void Start()
+    {
+        health.Initialize();
+        GetComponent<NavMeshAgent>().speed = speed;
+    }
+    private void Update()
+    {
+        nextAttack += Time.deltaTime;
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        if (Physics.Raycast(ray, out hit, sightRange, canSee))
+        {
+            if (faction == Faction.Player)
+            {
+                if (hit.collider.GetComponent<Unit>() && hit.collider.GetComponent<Unit>().faction == Faction.Enemy)
+                {
+                    GetComponent<NavMeshAgent>().enabled = false;
+                    transform.Find("Model").GetComponent<Animator>().SetBool("Walking",false);
+                    if (hit.collider.GetComponent<Unit>() && nextAttack >= attackCooldown)
+                    {
+                        hit.collider.GetComponent<Unit>().TakeDamage(attackDamage);
+                        transform.Find("Model").GetComponent<Animator>().SetTrigger("Attack");
+                        nextAttack = 0;
+                    }
+                }
+                else if (hit.collider.GetComponent<Base>() && hit.collider.GetComponent<Base>().faction == Faction.Enemy)
+                {
+                    if (hit.collider.GetComponent<Base>().health.CurrentVal > 0)
+                    {
+                        transform.Find("Model").GetComponent<Animator>().SetBool("Walking", false);
+                        GetComponent<NavMeshAgent>().enabled = false;
+                    }
+                    else if (hit.collider.GetComponent<Base>().health.CurrentVal <= 0)
+                    {
+                        GetComponent<NavMeshAgent>().enabled = true;
+                        GetComponent<NavMeshAgent>().SetDestination(destination.transform.position);
+                        transform.Find("Model").GetComponent<Animator>().SetBool("Walking", true);
+                    }
+                    if (hit.collider.GetComponent<Base>().health.CurrentVal > 0 && nextAttack >= attackCooldown)
+                    {
+                        hit.collider.GetComponent<Base>().TakeDamage(attackDamage);
+                        transform.Find("Model").GetComponent<Animator>().SetTrigger("Attack");
+                        nextAttack = 0;
+                    }
+                }
+            }
+            else if (faction == Faction.Enemy)
+            {
+                if (hit.collider.GetComponent<Unit>() && hit.collider.GetComponent<Unit>().faction == Faction.Player)
+                {
+                    GetComponent<NavMeshAgent>().enabled = false;
+                    transform.Find("Model").GetComponent<Animator>().SetBool("Walking", false);
+                    if (hit.collider.GetComponent<Unit>() && nextAttack >= attackCooldown)
+                    {
+                        hit.collider.GetComponent<Unit>().TakeDamage(attackDamage);
+                        transform.Find("Model").GetComponent<Animator>().SetTrigger("Attack");
+                        nextAttack = 0;
+                    }
+                }
+                else if (hit.collider.GetComponent<Base>() && hit.collider.GetComponent<Base>().faction == Faction.Player)
+                {
+                    if (hit.collider.GetComponent<Base>().health.CurrentVal > 0)
+                    {
+                        transform.Find("Model").GetComponent<Animator>().SetBool("Walking", false);
+                        GetComponent<NavMeshAgent>().enabled = false;
+                    }
+                    else if (hit.collider.GetComponent<Base>().health.CurrentVal <= 0)
+                    {
+                        GetComponent<NavMeshAgent>().enabled = true;
+                        GetComponent<NavMeshAgent>().SetDestination(destination.transform.position);
+                        transform.Find("Model").GetComponent<Animator>().SetBool("Walking", true);
+                    }
+                    if (hit.collider.GetComponent<Base>().health.CurrentVal > 0 && nextAttack >= attackCooldown)
+                    {
+                        hit.collider.GetComponent<Base>().TakeDamage(attackDamage);
+                        transform.Find("Model").GetComponent<Animator>().SetTrigger("Attack");
+                        nextAttack = 0;
+                    }
+                }
+            }
+        }
+        else if (!Physics.Raycast(ray, out hit, sightRange, canSee))
+        {
+            if (!GetComponent<NavMeshAgent>().enabled)
+            {
+                GetComponent<NavMeshAgent>().enabled = true;
+                if (destination)
+                    GetComponent<NavMeshAgent>().SetDestination(destination.transform.position);               
+            }
+            transform.Find("Model").GetComponent<Animator>().SetBool("Walking", true);
+        }
+        Debug.DrawRay(ray.origin, ray.direction, Color.red);
+    }
+    public void TakeDamage(int damage)
+    {
+        if (damage >= health.CurrentVal)
+        {
+            // Unit die
+            transform.Find("Model").GetComponent<Animator>().SetTrigger("Die");
+        }
+        else if (damage < health.CurrentVal)
+        {
+            health.CurrentVal -= damage;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (faction == Faction.Player)
+        {
+            if (other.CompareTag("Base"))
+            {
+                if (other.GetComponent<Base>().faction == Faction.Enemy || other.GetComponent<Base>().faction == Faction.None)
+                {
+                    if (other.GetComponent<Base>().meshRenderer)
+                    {
+                        other.GetComponent<Base>().meshRenderer.material = GameManager.Instance.playerBaseMaterial;
+                        other.GetComponent<Base>().auraRenderer.material = GameManager.Instance.playerAuraMaterial;
+                    }
+                    other.GetComponent<Base>().faction = Faction.Player;
+                    other.gameObject.layer = 8;
+                    if (GameManager.Instance.enemyBases.Contains(other.GetComponent<Base>()))
+                        GameManager.Instance.enemyBases.Remove(other.GetComponent<Base>());
+                    other.GetComponent<Base>().health.CurrentVal = other.GetComponent<Base>().health.MaxVal;
+                    Destroy(gameObject);
+                }
+                else if (other.GetComponent<Base>().faction == Faction.Player && other.GetComponent<Base>() != spawnBase)
+                {
+                    Destroy(gameObject);
+                    GameManager.Instance.playerUnitCooldown1.CurrentVal = GameManager.Instance.playerUnitCooldown1.MaxVal;
+                    GameManager.Instance.playerUnitCooldown2.CurrentVal = GameManager.Instance.playerUnitCooldown2.MaxVal;
+                    GameManager.Instance.playerUnitCooldown3.CurrentVal = GameManager.Instance.playerUnitCooldown3.MaxVal;
+                }
+            }
+        }
+        else if (faction == Faction.Enemy)
+        {
+            if (other.CompareTag("Base"))
+            {
+                if (other.GetComponent<Base>().faction == Faction.Player || other.GetComponent<Base>().faction == Faction.None)
+                {
+                    other.GetComponent<Base>().meshRenderer.material = GameManager.Instance.enemyBaseMaterial;
+                    other.GetComponent<Base>().auraRenderer.material = GameManager.Instance.enemyAuraMaterial;
+                    other.GetComponent<Base>().faction = Faction.Enemy;
+                    other.gameObject.layer = 9;
+                    GameManager.Instance.enemyBases.Add(other.GetComponent<Base>());
+                    other.GetComponent<Base>().health.CurrentVal = other.GetComponent<Base>().health.MaxVal;
+                    Destroy(gameObject);
+                }
+                else if (other.GetComponent<Base>().faction == Faction.Enemy && other.GetComponent<Base>() != spawnBase)
+                {
+                    Destroy(gameObject);
+                    GameManager.Instance.enemyUnitCooldown = 0;
+                }
+            }
+        }
+    }
+}
